345691448df8a48ec9b755462fad3f21ee371533
Jana Kerkhoff <jana1103k@googlemail.com>
2019-05-22 19:18:33 +0200

    - added a NN to trainNeuralNet.py - NN can be trained and model with weights are stored
    - added calculateMatchings.py
    	- loads the NN
    	- calculates the matching factor for a user and all other users

---
+++ neuralnetwork/scripts/calculateMatchings.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
+import numpy as np
+import csv
+from datetime import date
+
+
+def load_user_data_for_matching(filename):
+    """
+        Loads user data from given file
+
+        Parameters:
+        filename (string): file to load
+
+        Returns:
+        array: user data for all users
+    """
+
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+        data = np.array(data).astype("float")
+
+        return data
+
+
+def match(user_id):
+    """
+        Calculates the matching factors for user given by a user_id with all other registered users (excluding the user himself).
+
+        Parameters:
+        user_id (int): user to be matched
+
+        Returns:
+        list: list of all other user ids ordered descendingly by calculated matching factor. That is the user belonging to the first user id in the list
+                is the one that fits best to the user given by user_id
+    """
+
+    model_filename = "../data/model/2019-05-22_model.h5"
+    user_data_filename = "../data/testdata/testdatanumeric.csv"
+
+    # load NN
+    model = load_model(model_filename)
+
+    # load user data
+    user_data = load_user_data_for_matching(user_data_filename)
+
+    # create predictions for user_id
+    predictions = []
+    for i in range(user_data.shape[0]):
+        if i == user_id:
+            continue
+
+        input_data = np.array([user_data[user_id], user_data[i]])
+        input_data = input_data.reshape(1,10)
+        print(input_data, input_data.shape)
+        pred = model.predict(x=input_data)
+        predictions.append(tuple([i, pred]))
+
+    return predictions
+
+
+if __name__ == "__main__":
+    print(match(3))
--- neuralnetwork/scripts/trainNeuralNet.py
+++ neuralnetwork/scripts/trainNeuralNet.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
 import numpy as np
 import csv
 from datetime import date
@@ -30,7 +32,7 @@ def load_data(filename):
         return x_data, y_data
 
 
-def split_data(x, y, test_ratio = 0.2):
+def split_data(x, y, test_ratio=0.2):
     """
         Splits given data in training and test set
 
@@ -47,24 +49,23 @@ def split_data(x, y, test_ratio = 0.2):
     """
 
     # splitting index (rounded in favor of training data)
-    s = int(x.shape[0] * test_ratio) + 1
- 
+    s = int(x.shape[0] * (1 - test_ratio)) + 1
+
     # Create and randomly permute an index.
-    index = np.zeros(x.shape[0], dtype = bool)
+    index = np.zeros(x.shape[0], dtype=bool)
     index[:s] = True
     index = np.random.permutation(index)
- 
+
     return x[index], y[index], x[np.logical_not(index)], y[np.logical_not(index)]
 
 
-
-def train_network(data_filename, weights_filename):
+def train_network(data_filename, model_filename, epochs, batch_size):
     """
         Defines and trains the neural network using training data given by the specified file
 
         Parameters:
         data_filename (string): filename containing training data
-        weights_filename (string): filename to store trained weights of the NN
+        model_filename (string): filename to store the model and trained weights of the NN
 
         Returns:
         -
@@ -76,7 +77,23 @@ def train_network(data_filename, weights_filename):
     print("Splitting data in training and test set")
     X_train, Y_train, X_test, Y_test = split_data(X_data, Y_data)
 
-    print(X_train[0], Y_train[0])
+    # define the NN
+    num_user_data = 10
+    model = Sequential()
+
+    model.add(Dense(units=64, activation='relu',
+                    input_dim=num_user_data))
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=64, activation='relu'))
+    model.add(Dense(units=16, activation='relu'))
+    model.add(Dense(units=1, activation='relu'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    # train NN and store model
+    model.fit(x=X_train, y=Y_train, epochs=epochs, batch_size=batch_size)
+    model.save(model_filename)
 
     return
 
@@ -86,7 +103,8 @@ if __name__ == "__main__":
     # filename initial training data (first training of the NN)
     data_filename = "../data/testdata/initialtrainingdata.csv"
 
-    # filename to store trained NN weights
-    weights_filename = "../data/weights/{0}_weights.hdf5".format(date.today())
+    # filename to store model and trained NN weights
+    model_filename = "../data/model/{0}_model.h5".format(date.today())
 
-    train_network(data_filename, weights_filename)
+    train_network(data_filename=data_filename,
+                  model_filename=model_filename, epochs=100, batch_size=32)
