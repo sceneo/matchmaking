896ea9c4cde0b9262e5fa57b8002f03c72400ffa
Lea <Lea.Reckhordt@gmail.com>
2019-05-24 12:32:12 +0200

    adding some css files

--- src/PasswordForgotton.js
+++ src/PasswordForgotton.js
@@ -2,20 +2,19 @@
 
 
 import React, { Component } from "react";
+import { Button, Form } from "react-bootstrap";
+import "./PasswordForgotton.css";
 
 class PasswordForgotton extends Component {
   constructor(props) {
     super(props);
   }
-// title bar
-//      logout
-//      
-// central chat functionality
-//      https://medium.freecodecamp.org/how-to-build-a-react-js-chat-app-in-10-minutes-c9233794642b
-// links contact list (filled with ackend call tbd)
-// Match Me Button auf dem Boden der Seite zu Match Me funktionalität
+
 // # enter email
 //  # receive email with ne password
+  
+      requestEmail(){
+  }
   
   
   render() {
@@ -32,7 +31,7 @@ class PasswordForgotton extends Component {
               <Form.Control type="email" placeholder="Enter email"             
               onChange={this.handleChange}/>
             </Form.Group>
-            <Button className="RequestEmail" disabled={!this.state.requestEmail} onClick={this.requestEmail} >
+            <Button className="RequestEmail"  onClick={this.requestEmail} >
               Request Email
             </Button>
           </Form>     
