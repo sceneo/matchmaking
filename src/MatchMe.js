c5c20ae22632601c2543de108a3950c91c51689c
Lea <Lea.Reckhordt@gmail.com>
2019-05-24 12:14:39 +0200

    adding pw forgotten page details

---
+++ src/MatchMe.js
@@ -0,0 +1,7 @@
+# call backend service
+# get list of possible matches
+# for each match get user info
+# show match
+# swipe left or right
+# submit selection to backend
+# if click in middle, open chat 
