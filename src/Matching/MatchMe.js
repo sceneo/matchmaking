ab76e4ddec56f0be54665180315a5b0831b4fbb9
Jana Kerkhoff <jana1103k@googlemail.com>
2019-05-31 18:10:48 +0200

    Added MatchHandler.js 	- responsible for holding recommendation data in the frontend, provide recommendation user data and handle swipes 	- TODO: 		- REST calls for retrieving recommendation list including user data 		- REST calls for updating user data and training data according to swipe results
    Added Matcher.js
    	- this will be the react component displaying the current recommendation and allowing swipe actions
    	- TODO:
    		- everything

--- src/Matching/MatchMe.js
+++
@@ -1,7 +0,0 @@
-# call backend service
-# get list of possible matches
-# for each match get user info
-# show match
-# swipe left or right
-# submit selection to backend
-# if click in middle, open chat 
---
+++ src/Matching/Matcher.js
@@ -0,0 +1,23 @@
+# call backend service
+# get list of possible matches
+# for each match get user info
+# show match
+# swipe left or right
+# submit selection to backend
+# if click in middle, open chat 
+
+import React from 'reac';
+
+
+class MatchMe extends React.Component {
+
+    render() {
+        return(
+            <div>
+                Matching Frontend component
+            </div>
+        );
+    }
+}
+
+export default MatchMe
