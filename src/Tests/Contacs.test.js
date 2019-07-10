d8fd705086d6346a1a1cdb38cbe6a1639fd29440
Lea <Lea.Reckhordt@gmail.com>
2019-07-10 22:05:23 +0200

    Adding some minor tests

--- src/Tests/Contacs.test.js
+++ src/Tests/Contacs.test.js
@@ -1,10 +1,14 @@
 // This test mounts a component and makes sure that it didn’t throw during rendering
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
+import ReactDOM from 'react-dom';
+import Contacts from './../Chat/Contacts.js';
+
+it('renders without crashing', () => {});
+
+it('User s rooms can be requestd', () => {
+
+
     
-    // tests to be added...
-});
+  });
