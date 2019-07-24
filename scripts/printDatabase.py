9b94e10f912b6caa8060687badaddcbb22fdee66
Lea <Lea.Reckhordt@gmail.com>
2019-07-24 17:00:47 +0200

    screenshots added

--- scripts/printDatabase.py
+++ scripts/printDatabase.py
@@ -8,6 +8,7 @@ import time
 import boto3
 from symbol import except_clause
 from getpass import getuser
+import requests
 
 def getUsersFromS3():
     s3 = boto3.resource(u's3')
@@ -17,12 +18,29 @@ def getUsersFromS3():
     users = csv.DictReader(response[u'Body'].read().decode('utf-8').split())
     return users
 
+def getUsersWithGet():
+    headers = {'content-type' : 'application/json; utf-8'}
+    data = {"usecase": "detailsList"}
+    
+    url = "https://05vtryrhrg.execute-api.eu-west-1.amazonaws.com/Prod/MatchMakingAuth"
+    response = requests.post(url,params=json.dumps(data),headers=headers)
+    print(response.text)
+    print(response.status_code)
+    users = response.json();
+    return users
 
 def printAll():
     users = getUsersFromS3()
     for user in users:
         print(user)
 
+def printFromGet():
+    users = getUsersWithGet()
+    for user in users:
+        print(user)    
 
 if __name__ == '__main__':
     printAll()
+#     printFromGet();
+    
+
